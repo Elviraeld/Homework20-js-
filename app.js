4a933e111b4a8c866239fc0e1d4de13de3381f8d
Elviraeld <elyaali05@gmail.com>
2024-09-03 17:10:24 +0400

    task1

---
+++ app.js
@@ -0,0 +1,53 @@
+let month = prompt("ayin nomresini daxil edin");
+switch (month) {
+   case "1":
+   case "01":
+    console.log("Yanvar");
+    break;
+   case "2":
+   case "02":
+    console.log("Fevral");
+    break;
+   case "3":
+   case "03":
+    console.log("Mart");
+    break;
+   case "4":
+   case "04":
+    console.log("Aprel");
+    break;
+   case "5":
+   case "05":
+    console.log("May");
+    break;
+   case "6":
+   case "06":
+    console.log("Iyun");
+    break;
+   case "7":
+   case "07":
+    console.log("Iyul");
+    break;
+   case "8":
+   case "08":
+    console.log("Avqust");
+    break;
+   case "9":
+   case "09":
+    console.log("Sentyabr");
+    break;
+   case "10":
+    console.log("Oktyabr");
+    break;
+   case "11":
+    console.log("Noyabr");
+    break;
+   case "12":
+    console.log("Dekabr");
+    break;
+    default:
+        console.log("Bu nomrede ay movcud deyildir");
+        break;
+        
+
+}
